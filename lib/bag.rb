e6c441ba5341dd5d1df279bc6003829763110928
heybluguy <heybluguy@yahoo.com>
2017-10-20 00:54:29 -0600

    trick_or_treat

---
+++ lib/bag.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Bag
+  attr_reader :empty,:count
+  def initialize
+    @count = 0
+    @candies = []
+  end
+
+  def empty?
+    true
+  end
+
+  def candies
+    @candies
+  end
+
+  def <<(candy)
+    @candies << candy
+    @count += 1
+  end
+
+  def empty?
+    true  if @candies.count == 0
+  end
+
+  def contains?(candy)
+     candies.first.type.eql?(candy)
+  end
+end
